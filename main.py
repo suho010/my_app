43248d28089c37c27132d88878707408b315ad79
suho010 <24_11012@daejin.sen.hs.kr>
2025-01-08 11:18:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import streamlit as st
 
 st.title("나의 첫번째 앱")
-
 st.text('\n\n')
-
 st.write('안녕하세요. 저는 이수호입니다.')
 st.write('저의 이메일 주소는 24_11012@daejin.sen.hs.kr')
+
+st.button("Reset", type="primary")
+if st.button("Say hello"):
+    st.write("Why hello there")
+else:
+    st.write("Goodbye")
+
+if st.button("Aloha", type="tertiary"):
+    st.write("Ciao")
